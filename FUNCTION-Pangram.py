414a6a2c507bb1382894158607ecc26ed5862c78
Swetha-byte <swetharao1104@gmail.com>
2024-07-03 16:09:17 +0100

    add new pangram file

---
+++ FUNCTION-Pangram.py
@@ -0,0 +1,45 @@
+
+## PANGRAM CHANGE: ##                                      (  165  )
+
+
+# def pangram(phrase):
+    
+#     letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+#     phr = set(phrase)
+    
+#     if len(phr) >=  len(letters):
+#         return True
+#     else:
+#         return False
+    
+# p1 = pangram('the quick brown fox jumps over the lazy dog')
+# print(p1)   
+
+
+## CASE COUNTING LETTERS : ##                              (  166  )
+
+# def count(phrase):
+    
+#       low=0
+#       up =0 
+#       for i in phrase:
+#           if i.islower():
+#                low = low +1
+               
+#           else:
+#                up= up +1
+
+#       return low , up  
+
+# print(count('ABdcjSAKJE'))          
+
+
+
+
+
+
+
+
+
+
+       
