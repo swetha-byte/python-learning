a576c8509ed9b23b765006dc4dee53c5e1183cd3
Swetha-byte <swetharao1104@gmail.com>
2024-07-05 15:34:30 +0100

    add1

--- FUNCTION-Pangram.py
+++ FUNCTION-Pangram.py
@@ -1,5 +1,5 @@
 
-## PANGRAM CHANGE: ##                                      (  165  )
+## PANGRAM CHANGE: ##                                                             (  165  )
 
 
 # def pangram(phrase):
@@ -16,7 +16,7 @@
 # print(p1)   
 
 
-## CASE COUNTING LETTERS : ##                              (  166  )
+## CASE COUNTING LETTERS : ##                                                            (  166  )
 
 # def count(phrase):
     
@@ -35,10 +35,35 @@
 
 
 
+###     MINIMUM VARIABLE NUMBER:                                                       (  167  )
+
+# ##  min (1,5,7,-5,6)  ...............  # (return -5)
+
+# def min( *val, low_limit= None):
+#     if low_limit is None:
+#         return min(val1)
+#     else:
+#         l=[x for x in val if x >= low_limit ]
+#         return l
+    
+# val1 =min(1,5,4,7,-5,4) 
+# print(val1)   
 
 
 
+##  PASCAL'S TRAINGLE:                                                       (  168  )
 
+# def pascal(n):
+#     r = [1]
+
+#     for i in range(n):
+#         print(r)
+#         tr = [0]+r
+#         r = r +[0]
+#         nr = [ x+y for x,y in zip(r,tr)]
+#         r = nr
+
+# pascal(6) 
 
 
 
