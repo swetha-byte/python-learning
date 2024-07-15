d76e04687a215fd4f672ac1e69f82f1cf69a67bc
Swetha-byte <swetharao1104@gmail.com>
2024-07-15 19:47:15 +0100

    AccBlnc

--- Exceptions.py
+++ Exceptions.py
@@ -1,4 +1,4 @@
-# ###  NEGATIVE AGE: ###
+# ###  NEGATIVE AGE: ###                                   (  187  )
 
 # def stage(age):
 
@@ -14,7 +14,7 @@
 #         return 'old'
    
 
-# age = int(input('enter age: '))
+# age = int(input('enter age: '))                                           
 
 # try:
 #     age1 = stage(age)
@@ -25,27 +25,82 @@
 
 
 
-class NegativeAgeException(Exception):
-    pass
+# class NegativeAgeException(Exception):                    (   187-a   )
+#     pass
 
-def stage(age):
+# def stage(age):
 
-    if age < 0:
-        raise NegativeAgeException('Age should not be negative')
-    elif  age <= 13:
-        return 'kid'
-    elif age <=19:
-        return 'teen'
-    elif age < 50:
-        return 'young'
-    else:
-        return 'old'
+#     if age < 0:
+#         raise NegativeAgeException('Age should not be negative')
+#     elif  age <= 13:
+#         return 'kid'
+#     elif age <=19:
+#         return 'teen'
+#     elif age < 50:
+#         return 'young'
+#     else:
+#         return 'old'
    
 
-age = int(input('enter age: '))
+# age = int(input('enter age: '))
+
+# try:
+#     age1 = stage(age)
+#     print(age1)
+# except NegativeAgeException as e:
+#     print(e)   
+
+
+
+
+
+
+# ###  ACCOUNT BALANCE ###                                        (  188  )
+
+# class AccountBalanceException(Exception):
+#     pass
+
+# balance = 10000
+
+# def Withdraw(amt):
+#     global balance
+#     WD = balance - amt
+    
+
+#     if WD <= 5000:
+#         raise AccountBalanceException('minimum balance should be 5000')
+#     else:
+#         print(WD)
+    
+
+# try:
+#     Withdraw(4000)
+# except AccountBalanceException as msg:
+#     print(msg)
+    
+
+
+
+
+class AccounbalanceException(Exception):
+    pass
+
+balance=10000
+
+def withdraw(amt):
+    global balance
+
+    if balance - amt < 5000:
+        raise AccounbalanceException('minimum amt should be 5000')
+    else :
+        b = balance-amt
+        print('Remaining amt is' , b)
 
 try:
-    age1 = stage(age)
-    print(age1)
-except NegativeAgeException as e:
-    print(e)   
+    f = withdraw(int(input('enter amt: ')))
+    print(f)
+except AccounbalanceException as e:
+    print(e)
+
+
+
