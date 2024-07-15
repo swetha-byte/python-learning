4055b4220ccb66ddb8b073203d06f4fb9e4fd0e8
Swetha-byte <swetharao1104@gmail.com>
2024-07-15 18:28:30 +0100

    exception

--- Exceptions.py
+++ Exceptions.py
@@ -1,9 +1,37 @@
-###  NEGATIVE AGE: ###
+# ###  NEGATIVE AGE: ###
+
+# def stage(age):
+
+#     if age < 0:
+#         raise 'NegativeAgeError'
+#     elif  age <= 13:
+#         return 'kid'
+#     elif age <=19:
+#         return 'teen'
+#     elif age < 50:
+#         return 'young'
+#     else:
+#         return 'old'
+   
+
+# age = int(input('enter age: '))
+
+# try:
+#     age1 = stage(age)
+#     print(age1)
+# except:
+#     print('negative age exception')    
+
+
+
+
+class NegativeAgeException(Exception):
+    pass
 
 def stage(age):
 
     if age < 0:
-        raise 'NegativeAgeError'
+        raise NegativeAgeException('Age should not be negative')
     elif  age <= 13:
         return 'kid'
     elif age <=19:
@@ -19,5 +47,5 @@ age = int(input('enter age: '))
 try:
     age1 = stage(age)
     print(age1)
-except:
-    print('negative age exception')    
+except NegativeAgeException as e:
+    print(e)   
