68404197dea5a635e0c67a3e15ebe259800b7df7
Swetha-byte <swetharao1104@gmail.com>
2024-08-08 10:52:39 +0100

    using semaphore

--- MULTITHREADING.py
+++ MULTITHREADING.py
@@ -299,33 +299,54 @@
 
 
 
-###                        (  MUTEX  )                       ##
+# ###                        (  MUTEX  )                       ##
+
+
+# from threading import*
+# from time import*
+
+# def display(str1):
+#     l.acquire()
+#     for x in str1:
+#         print(x)
+#     l.release()
+
+
+# l = Lock()
+# t1 = Thread(target=display,args=('LEARNING PYTHON ',))
+# t2 = Thread(target=display,args=('you are welcome ',))
+
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+
+
+
+
+###        (       SEMAPHORE      )   ###
 
 from threading import*
 from time import*
 
 def display(str1):
+
     l.acquire()
     for x in str1:
         print(x)
     l.release()
 
-
-l = Lock()
-t1 = Thread(target=display,args=('LEARNING PYTHON ',))
+l = Semaphore(2)
+t1 = Thread(target=display,args=('HELLO WORLD ',))        
 t2 = Thread(target=display,args=('you are welcome ',))
+t3 = Thread(target=display,args=('PYTHON LEARNING ',))
 
 t1.start()
 t2.start()
+t3.start()
 
 t1.join()
 t2.join()
-
-
-
-
-
-
-
-
-
+t3.join()
