cff50b10c5530167756f0f487011146f2c2a1587
Swetha-byte <swetharao1104@gmail.com>
2024-08-12 17:39:38 +0100

    calulating age

--- DATE & TIME.py
+++ DATE & TIME.py
@@ -53,8 +53,21 @@
 
 
 
+###  CALCULATE AGE:              (   250  )
 
+from datetime import date
 
+def age(dob):
+
+    today = date.today()
+    years = today.year - dob.year
+
+    if (today.month , today.day) < (dob.month , dob.day):
+        years -= 1
+
+    return years
+  
+print('Age:',age(date(1998,4,11)))
 
 
 
