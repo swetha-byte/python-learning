ee68741d592f5583f00c05accbc69e18b86051ed
Swetha-byte <swetharao1104@gmail.com>
2024-08-12 16:51:24 +0100

    str to obj date

--- DATE & TIME.py
+++ DATE & TIME.py
@@ -1,22 +1,53 @@
-# from datetime import*
-import datetime
-year = int(input('enter year : '))
+###  MONTHS STRATING FROM MONDAY :             (  247  ) 
 
-monday = 0
-for month in range(1,13):     
-    dt = datetime.date(year,month,1)
+# # from datetime import*
+
+# import datetime
+# year = int(input('enter year : '))
+
+# monday = 0
+# for month in range(1,13):     
+#     dt = datetime.date(year,month,1)
         
-    if dt.weekday() == 0:
-        monday += 1
-        print(month)
+#     if dt.weekday() == 0:
+#         monday += 1
+#         print(month)
 
-print('number of months starting with monday :',monday )           
-
-
+# print('number of months starting with monday :',monday )           
 
 
 
 
+
+###  CODE EXECUTION TIME:            (   248  )
+
+# import time
+
+# start_time =time.time()
+
+# for i in range(100000000):
+#     pass
+
+# end_time = time.time()
+
+# ex_time = end_time - start_time
+
+# print(ex_time)
+
+
+
+
+
+
+###   STRING TO OBJECT DATE:           (  249  )
+
+import datetime
+
+str_date = input('enter date in the form of DD-MM-YYYY : ')
+
+d,m,y = str_date.split('-')
+d1 = datetime.date(int(y),int(m),int(d))
+print(d1)
 
 
 
